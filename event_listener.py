77b6e91c50227e8155ebbbdcee5f2569d2ff8742
kieChang <kie4280ann@gmail.com>
2019-04-28 16:02:32 +0800

    change max queue size to finite

--- event_listener.py
+++ event_listener.py
@@ -8,62 +8,63 @@ class eventListener:
     listenerWorks = dict()
     threads = 0
 
-    def __init__(self, refresh_rate=0.01):
+    def __init__(self, refresh_rate=0.01, maxQueueSize=30):
         eventListener.threads = eventListener.threads+1
         self.refresh_rate = refresh_rate
         self.loop = True
-        self.workQueue = queue.Queue()
+        self.workQueue = queue.Queue(maxsize=maxQueueSize)
         self.dict_lock = threading.Lock()
         self.queue_lock = threading.Lock()
         self.workEvent = threading.Event()
-        self.thread_listen = threading.Thread(target=self.runListener, daemon=True)
+        self.thread_listen = threading.Thread(
+            target=self.runListener, daemon=True)
         self.thread_listen.start()
-        self.thread_callback = threading.Thread(target=self.runCallback, daemon=True)
-        self.thread_callback.start()     
-        
+        self.thread_callback = threading.Thread(
+            target=self.runCallback, daemon=True)
+        self.thread_callback.start()
 
         pass
 
     def runCallback(self):
         while self.loop:
             self.workEvent.wait()
-            self.queue_lock.acquire()
-            try:                
-                call, kwargs = self.workQueue.get()
-                self.workQueue.task_done()
+            # self.queue_lock.acquire()
+            try:
+                call, kwargs = self.workQueue.get()                
                 call(**kwargs)
+                self.workQueue.task_done()
 
             except Exception as e:
                 print(e)
             finally:
+                self.queue_lock.acquire()
                 if self.workQueue.qsize() == 0:
                     self.workEvent.clear()
-                if self.queue_lock.locked():
-                    self.queue_lock.release()
-            
+                self.queue_lock.release()
+                # if self.queue_lock.locked():
+                #     self.queue_lock.release()
+
             pass
-        
+
     def addCallback(self, callback, kwargs):
         self.queue_lock.acquire()
         try:
-            self.workQueue.put((callback, kwargs))
-        except:
+            self.workQueue.put((callback, kwargs), False)
+        except queue.Full:
+            print("Max queue size reached, the callback takes too long to run. \
+            Another thread may be needed.")            
             pass
         finally:
             self.workEvent.set()
             if self.queue_lock.locked():
                 self.queue_lock.release()
-        
 
     def runListener(self):
         # print(time.perf_counter())
 
         while self.loop:
 
-            start = time.perf_counter()
-            self.queue_lock.acquire()                      
-                
-            self.queue_lock.release()
+            start = time.perf_counter()            
             self.dict_lock.acquire()
             try:
                 for _, work in self.listenerWorks.items():
@@ -79,16 +80,13 @@ class eventListener:
                         return_dict = dict(work[3])
                         if(len(param_dict) > 0):
                             return_dict.update(param_dict)
-                        # work[1](**return_dict)
-                        # threading.Thread(
-                        #     target=work[1], kwargs=return_dict).start()
+                  
                         self.addCallback(work[1], return_dict)
-                        
-                        # threading event 
+                        print("add callback")                      
 
             except:
                 pass
-            finally:                
+            finally:
                 if self.dict_lock.locked():
                     self.dict_lock.release()
             # print(time.perf_counter()-start)
@@ -125,10 +123,10 @@ class eventListener:
             self.dict_lock.release()
 
     def __del__(self):
-        self.loop=False
-        
+        self.loop = False
+
         pass
-    
+
     def reset(self):
         pass
 
@@ -138,32 +136,33 @@ class eventScheduler:
         pass
 
 
-e = eventListener(1)
+e = eventListener(0.5, 3)
 i = 0
 
 loop = True
 
 L1 = None
+
+
 def check():
     global i
     i += 1
     # print(a ,b)
     if(i == 4):
+        i = 0
         return True, {"pressed": True}
     else:
         return False
 
 
 def callback(pressed):
-    print("event fired")
-    print(pressed)
-    # print(a ,b)
     global loop, L1, i
-    e.removeListener(L1)
-    L1 = e.addListener(check, callback)
-    print("removed")
-    i=0
+    
+    print("event fired")      
 
+    for a in range(10):
+        time.sleep(1)
+    # print(a ,b)    
 
 L1 = e.addListener(check, callback)
 while loop:
